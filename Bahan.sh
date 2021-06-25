6a07a20bd8aa598abcb5fa65e3f2009629ce2df7
Risky Solihin <santuyaja019@gmail.com>
2021-06-25 21:59:14 +0700

    Script Dalam Kendala Karena Hp Mimin Rusak

---
+++ Bahan.sh
@@ -0,0 +1,35 @@
+#utf-conding-bash
+#BahanNgab
+
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pkg install figlet -y
+pkg install toilet -y
+pkg install php -y
+pkg install bash -y
+pkg install nano -y
+pkg install mc -y
+pkg install pyfiglet -y
+pkg install wget -y
+pip install requests
+pip install bs4
+pip install futures
+pip2 install requests
+pip2 install bs4
+pip2 install futures
+clear
+
+
+echo
+echo
+echo
+echo
+echo "Install Bahan Selesai :)"
+echo "Ketik : python Start.py"
+echo
+echo
+echo
+
---
+++ modul/__init__.py
@@ -0,0 +1,128 @@
+import requests as r, re, os
+from bs4 import BeautifulSoup as par
+import platform
+import requests, bs4, sys, os, subprocess, random, time, re, json
+import concurrent.futures
+from datetime import datetime
+from time import sleep
+from requests import Session
+import re, sys
+import sys
+from os import system
+import os, sys, time, random
+from sys import exit as keluar
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from sys import stdout
+from os import system
+import re
+import os,random,time,sys
+import json
+try: import requests
+except ModuleNotFoundError: os.system("python -m pip install requests &> /dev/null")
+try: import bs4
+except ModuleNotFoundError: os.system("python -m pip install bs4 &> /dev/null")
+import requests as req
+from time import sleep as waktu
+from bs4 import BeautifulSoup as parser
+current = datetime.now()
+import requests,mechanize,bs4,sys,os,subprocess,uuid,random,time,re,base64,concurrent.futures,json
+koneksi_error=(req.exceptions.ConnectionError,req.exceptions.ChunkedEncodingError,req.exceptions.ReadTimeout)
+#Jaga Jarak Kawan :v
+ua = {"user-agent":"chrome"}
+prvt = []
+ses = r.Session()
+link = "https://free.facebook.com/"
+#Memek Kau :p
+N="\033[00m"
+q="\033[00m"
+h2="\033[40m"
+b2="\033[44m"
+c2="\033[46m"
+i2="\033[42m"
+u2="\033[45m"
+m2="\033[41m"
+p2="\033[47m"
+k2="\033[43m"
+b='\033[0;34m'
+i='\033[0;32m'
+c='\033[0;36m'
+m='\033[0;31m'
+u='\033[0;35m'
+k='\033[0;33m'
+p='\033[0;37m'
+h='\033[0;90m'
+b='\033[0;34m'
+I='\033[0;32m'
+C='\033[0;36m'
+M='\033[0;31m'
+U='\033[0;35m'
+K='\033[0;33m'
+P='\033[0;37m'
+H='\033[0;90m'
+warna_me=([i, c, m, u, k, p, h, b])
+warna_ms=([i, c, m, u, k, p, h, b])
+w2=(warna_me)
+w=pilih(w2)
+
+kosong1="Anda Tau Kontol Engga"
+kosong2="Kosong...???"
+pilih1="Pilihan Ini Tidak Terdaftar !!"
+pilih2="Menu Pilihan Anda Tidak Ada Pantem"
+tunggu1="Tunggu Bentar Ngab"
+def kembali(kata,fuck=None):
+        print(kata)
+        waktu(2)
+        fuck() if fuck is not None else exit()
+def head(url,respons):
+        return {"Host":url.split("//")[1],"upgrade-insecure-requests":"1","cache-control":"max-age=0","content-type":"application/x-www-form-urlencoded","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","referer":respons.url,"accept-encoding":"gzip, deflate, br","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","user-agent":"Mozilla/5.0 (Linux; Android 5.1.1; SM-G600S Build/LMY47V; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36"}
+def kntl():
+#                 %s"[ K N T L ]"
+        lpp___1 =("%s[          "%(k))
+        lpp___2 =("%s[ %sK        "%(k, m))
+        lpp___3 =("%s[ %sK %sN      "%(k, m, u))
+        lpp___4 =("%s[ %sK %sN %sT    "%(k, m, u, c))
+        lpp___5 =("%s[ %sK %sN %sT %sL "%(k, m, u, c, b))
+        lpp___6 =("%s[ %sK %sN %sT %sL %s]"%(k, m, u, c, b, k))
+        lpp___7 =("%s[ %sK %sN %sT %sL %s]"%(k, m, u, c, b, k))
+        lpp___8 =("%s[ %sK %sN %sT %sL "%(k, m, u, c, b))
+        lpp___9 =("%s[ %sK %sN %sT    "%(k, m, u, c))
+        lpp___10 =("%s[ %sK %sN      "%(k, m, u))
+        lpp___11 =("%s[ %sK        "%(k, m))
+        lpp___12 =("%s           "%(k))
+        lpp___13 =("%s[         ]"%(k))
+        lpp___14 =("  %sK %sN %sT %sL  "%(m, u, c, b,))
+        lpp___15 =("%s[          "%(k))
+        lpp___16 =("%s          ]"%(k))
+        lpp___17 =("%s[ %sD %sO %sN %sE %s]"%(k, i, i, i, i, k))
+
+        lpp___all = ([lpp___1, lpp___2, lpp___3, lpp___4, lpp___5, lpp___6,lpp___7, lpp___8, lpp___9,lpp___10, lpp___11, lpp___12, lpp___13,lpp___14, lpp___13, lpp___14, lpp___13, lpp___14, lpp___13, lpp___14, lpp___15, lpp___16, lpp___17])
+        for x in lpp___all:
+            stdout.write(f'\r %s[%s{w}LPP%s]--->> %s'%(N,w,N,x)),
+            stdout.flush()
+logo2 = (f"""{u}{i}<<{h}--------------------------------------------------------------{i}>>
+________                 ______  _________________ ________________
+___  __ \______ ____________  /_____  ____/___    |__  ___/___  __/
+__  / / /_  __ `/__  ___/__  //_/__  /_    __  /| |_____ \ __  /   
+_  /_/ / / /_/ / _  /    _  ,<   _  __/    _  ___ |____/ / _  /    
+/_____/  \__,_/  /_/     /_/|_|  /_/       /_/  |_|/____/  /_/     
+{i}<<{h}--------------------------------------------------------------{i}>>
+{p}Version{h}----{p}>>{c}Version 3.5.2
+{p}Athour{h}-----{p}>>{c}Mr.Risky
+{p}Github{h}-----{p}>>{c}github.com/Dumai-991
+{p}GuruKu{h}-----{p}>>{c}Dapunta,Angga, And Yayan
+{p}WhatsAPP{h}---{p}>>{c}6283143565470
+{p}Telegram{h}---{p}>>{c}6283143565470
+{p}Facebook{h}---{p}>>{c}m.facebook.com/llovexnxx
+{i}<<{h}--------------------------------------------------------------{i}>>""")
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(1./500)
+def jalan2(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(1./140)
